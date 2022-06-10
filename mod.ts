868b8ac54f3ca3e6aec7ef2b67625a469b5d2655
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-06-10 16:44:01 -0700

    mod file with oak

---
+++ mod.ts
@@ -0,0 +1,11 @@
+import { Application } from "https://deno.land/x/oak/mod.ts";
+
+const app = new Application();
+
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.headers.get("X-Response-Time");
+  console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
+});
+
+await app.listen({ port: 8000 });
