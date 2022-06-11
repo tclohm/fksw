c790f9632f5e15eb4b8b082de86de57f7282ffd9
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-06-11 09:55:10 -0700

    timing middleware

--- mod.ts
+++ mod.ts
@@ -8,4 +8,11 @@ app.use(async (ctx, next) => {
   console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
 });
 
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.response.headers.set("X-Response-Time", `${ms}ms`);
+});
+
 await app.listen({ port: 8000 });
